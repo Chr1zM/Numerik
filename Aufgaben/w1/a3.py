ca790447b4ff044879c4e1b250eeea3c3d90a3ca
Chr1zM <ch.me010103@gmail.com>
2023-03-29 21:10:12 +0200

    A3

--- Aufgaben/w1/a3.py
+++ Aufgaben/w1/a3.py
@@ -9,11 +9,11 @@ def rechteckSum(f, a, b, n):
 # komische Werte bei Trapezregel..
 def trapezregel(f, a, b, n):
     h = (b - a) / n
-    return (h / 2) * (f(a) + 2 * sum(f(a + i * h) + f(b) for i in range(n)))
+    return h / 2 * (f(a) + 2 * sum(f(a + i * h) for i in range(1, n)) + f(b))
 
 
 f1 = lambda x: 1 / (x ** 2)
-f2 = lambda x: math.log2(x)
+f2 = lambda x: math.log(x)
 
 f1resExact = 9.9
 f2resExact = 0.38629
@@ -22,14 +22,16 @@ print("Exakte Werte:")
 print("f1: " + str(f1resExact))
 print("f2: " + str(f2resExact) + "\n")
 
-for n in [100, 200, 500, 1000, 10000, 100000]:
+for n in [10, 50, 100, 500]:
     print("n = " + str(n))
 
     print("f1: rechteck=" + str(rechteckSum(f1, 1 / 10, 10, n)))
     print("f1: trapez=" + str(trapezregel(f1, 1 / 10, 10, n)))
     print("---------------------------------")
 
-for n in [1, 5, 10, 20, 50, 100, 200, 500, 1000, 10000, 100000]:
+print("\n")
+
+for n in [10, 50, 100, 500]:
     print("n = " + str(n))
     print("f2: rechteck=" + str(rechteckSum(f2, 1, 2, n)))
     print("f2: trapez=" + str(trapezregel(f2, 1, 2, n)))
