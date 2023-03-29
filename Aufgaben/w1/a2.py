a1025bacf927047a8eb4ea3191ba3d3b1450033d
Christian Meyer <ch.me010103@gmail.com>
2023-03-29 20:03:04 +0200

    Initialer Commit + Woche 1 A1, A2

---
+++ Aufgaben/w1/a2.py
@@ -0,0 +1,23 @@
+import numpy
+import matplotlib.pyplot
+import math
+
+
+def exp(x, n):
+    return sum(math.pow(x, k) / math.factorial(k) for k in range(n + 1))
+
+
+def expAlt(x, n):
+    return sum(math.pow(x, n - k) / math.factorial(n - k) for k in range(n + 1))
+
+
+for x in [1, 2, 5, 25]:
+    res = numpy.array([exp(x, n) for n in range(1, 40)])
+    resAlt = numpy.array([expAlt(x, n) for n in range(1, 40)])
+
+    matplotlib.pyplot.plot(range(1, 40), res, label="exp", linewidth=4, color='r')
+    matplotlib.pyplot.plot(range(1, 40), resAlt, label="expAlt", color='blue')
+    matplotlib.pyplot.ylabel("f(x)")
+    matplotlib.pyplot.xlabel("n")
+    matplotlib.pyplot.legend(["exp", "expAlt"])
+    matplotlib.pyplot.show()
