dd7f3e97d320bfd4924f74fe8d76d544fb6a6f54
Chr1zM <ch.me010103@gmail.com>
2023-03-30 20:05:33 +0200

    A1

--- Aufgaben/w1/a1.py
+++ Aufgaben/w1/a1.py
@@ -3,19 +3,19 @@ import math
 
 def pqFormel(p, q):
     d = math.sqrt(p * p + q)
-    return -p - d, -p + d
+    return max(-p - d, -p + d)
 
 
 def pqFormelAlt(p, q):
     x1 = -p - math.sqrt(p * p + q)
     x2 = -q / x1
-    return x1, x2
+    return max(x1, x2)
 
 
 q = 1
 for p in [math.pow(10, 2), math.pow(10, 4), math.pow(10, 6), math.pow(10, 7), math.pow(10, 8)]:
     result = pqFormel(p, q)
     altResult = pqFormelAlt(p, q)
-    print("pq: x1=" + str(result[0]) + " x2=" + str(result[1]))
-    print("pqAlt: x1=" + str(altResult[0]) + " x2=" + str(altResult[1]))
-    print("diff: (" + str(result[0]-altResult[0]) + ", " + str(result[1]-altResult[1]) + ")\n")
+    print("pq: x=" + str(result))
+    print("pqAlt: x=" + str(altResult))
+    print("diff: " + str(abs(result-altResult)) + "\n")
