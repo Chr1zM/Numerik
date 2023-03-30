74e8aca583686d22cd20e8b1396fb7ce147a9402
Chr1zM <ch.me010103@gmail.com>
2023-03-30 19:33:39 +0200

    A5

--- Aufgaben/w1/a5.py
+++ Aufgaben/w1/a5.py
@@ -13,7 +13,7 @@ def y_k(k, x, x0):
     return y_k(k - 1, x, x0) + a_k(k, x, x0)
 
 
-def anzSchritte(x, x0):
+def anz_schritte(x, x0):
     eps = 0.005
     i = 0
     while True:
@@ -26,12 +26,12 @@ def anzSchritte(x, x0):
 
 x = 2
 exact = math.sqrt(x)
-res1 = y_k(100, x, 1)
-res2 = y_k(100, x, 4)
+res1 = y_k(50, x, 1)
+res2 = y_k(50, x, 4)
 
 print("exact=" + str(exact))
-print("Wurzelannäherung mit i=500, ai=1, x=2 und x_0=1: " + str(res1))
-print("Wurzelannäherung mit i=500, ai=1, x=2 und x_0=4: " + str(res2))
+print("Wurzelannäherung mit k=50, x=2 und x_0=1: " + str(res1))
+print("Wurzelannäherung mit k=50, x=2 und x_0=4: " + str(res2))
 
-print("5a.2: mit ai=1, x=2 und x_0=1: " + str(anzSchritte(x, 1)) + " Schritte werden benötigt.")
-print("5a.2: mit ai=1, x=2 und x_0=1: " + str(anzSchritte(x, 4)) + " Schritte werden benötigt.")
+print("5a.2: mit x=2 und x_0=1: " + str(anz_schritte(x, 1)) + " Schritte werden benötigt.")
+print("5a.2: mit x=2 und x_0=1: " + str(anz_schritte(x, 4)) + " Schritte werden benötigt.")
