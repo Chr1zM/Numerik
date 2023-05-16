dcff01fd61a535d3dd1e207b71ed9fa3041c99d3
Chr1zM <ch.me010103@gmail.com>
2023-05-16 17:14:43 +0200

    W7 * A1 u. A4

--- Aufgaben/w7/a3.py
+++ Aufgaben/w7/a3.py
@@ -1,8 +1,9 @@
 import math
 
-import numpy as np
 import scipy.sparse
 import scipy.sparse.linalg
+
+from Blatt07_lib import *
 
 
 def sign(x):
@@ -15,12 +16,12 @@ def square_sum_of_subdiagonal(a):
     return np.sum(a_ ** 2)
 
 
-def largest_not_diagonal(a, n):
+def largest_not_diagonal(a, n_):
     a_ = abs(a.copy())
     np.fill_diagonal(a_, 0)
     mx = np.argmax(a_)
-    i = mx // n
-    j = mx % n
+    i = mx // n_
+    j = mx % n_
     return i, j
 
 
@@ -29,7 +30,7 @@ def jacobi_ew(a, sorted=True):
     assert (a.shape == (n, n))
     Q = np.eye(n, n)
     i = 0
-    while (square_sum_of_subdiagonal(a) > 1e-3):
+    while square_sum_of_subdiagonal(a) > 1e-3:
         i += 1
         if i % 100 == 0:
             print(square_sum_of_subdiagonal(a))
@@ -49,3 +50,18 @@ def jacobi_ew(a, sorted=True):
         ew = ew[ind]
         Q = Q[:, ind]
     return ew, Q
+
+
+if __name__ == '__main__':
+    m = 10
+    n = m ** 2
+    A = Ablock(m)
+    ew_jac, Q = jacobi_ew(A)
+
+    ew_ex = ew_exakt(m)
+
+    # compare eigenvalues
+    plt.semilogy(ew_jac[::-1], 'r.', label='Jacobi')
+    plt.semilogy(ew_ex, 'b.', label='Exakt')
+    plt.legend()
+    plt.show()
