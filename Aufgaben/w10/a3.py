2bf9db98ef16c42bb6f82603f0f898bb47293cc3
Chr1zM <ch.me010103@gmail.com>
2023-05-30 19:39:35 +0200

    W10 * A3

--- Aufgaben/w10/a3.py
+++ Aufgaben/w10/a3.py
@@ -1,44 +1,39 @@
-import numpy as np
-import math
 import matplotlib.pyplot as plt
+import numpy as np
 
-gamma = 0.05
-n = 100
-c = 1/(gamma * math.sqrt(2*math.pi))
-A = np.empty((n, n))
-for i in range(n):
-    for j in range(n):
-        A[i, j] = c/n * math.exp(-((i-j)/(math.sqrt(2)*n*gamma))**2)
-x = np.zeros((n,))
-for i in range(45, 56):
-    x[i] = 1
-for i in range(60, 66):
-    x[i] = 1/2
-b = A @ x
-b_stoerung = np.random.randn(n)/1e-6
-b_ = b + b_stoerung
-
-pseudoinverse = np.linalg.pinv(A)
-x_ = pseudoinverse @ b_
-u, s, vt = np.linalg.svd(A, )
 
 def get_tsvd_index(s, alpha):
-    n = s.shape[0]
-    assert (s.shape == (n,))
-    for i in range(1, n):
-        if s[0] / s[i] > 1 / alpha:
-            return i
+    return np.argmax(s[0] / s[1:] > 1 / alpha)
 
-get_tsvd_index(s, 1e-2)
-
-fig, axs = plt.subplots(3, 3, figsize=(16, 16))
-axs = axs.flatten()
-for i, alpha in enumerate(10**(-k) for k in range(0, 9)):
-    ind = get_tsvd_index(s, alpha)
-    sigma_ = 1/s
-    sigma_[ind:] = 0
-    x_ = vt.T @ np.diag(sigma_) @ u.T @b
-    axs[i].plot(x_)
-    axs[i].set_title(f'$\\alpha$={alpha}')
 
 if __name__ == '__main__':
+    gamma = 0.05
+    n = 100
+    c = 1 / (gamma * np.sqrt(2 * np.pi))
+
+    A = c / n * np.exp(-((np.arange(n)[:, None] - np.arange(n)) / (np.sqrt(2) * n * gamma)) ** 2)
+
+    x = np.zeros(n)
+    x[45:56] = 1
+    x[60:66] = 1 / 2
+
+    b = A @ x
+    b_ = b + np.random.randn(n) / 1e-6
+
+    u, s, vt = np.linalg.svd(A)
+    ind = get_tsvd_index(s, 1e-2)
+    sigma_ = 1 / s
+    sigma_[ind:] = 0
+    x_ = vt.T @ np.diag(sigma_) @ u.T @ b
+
+    fig, axs = plt.subplots(3, 3, figsize=(16, 16))
+
+    for i, alpha in enumerate(10 ** (-k) for k in range(9)):
+        ind = get_tsvd_index(s, alpha)
+        sigma_[:ind] = 1 / s[:ind]
+        sigma_[ind:] = 0
+        x_ = vt.T @ np.diag(sigma_) @ u.T @ b
+        axs.flatten()[i].plot(x_)
+        axs.flatten()[i].set_title(f'$\\alpha$={alpha}')
+
+    plt.show()
